d7fda0b30c69ade8312bb9cdac2d1d8f09e8428d
Yhannpiercruz <yhannpiercruz@gmail.com>
2026-06-01 16:14:37 -0500

    Fix: force dns resolution ipv4 for clever cloud

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,5 +1,9 @@
 const mysql = require('mysql2');
+const dns = require('dns');
 require('dotenv').config();
+
+// Forzar a Node.js a resolver las direcciones DNS correctamente en entornos de producción
+dns.setDefaultResultOrder('ipv4first');
 
 const pool = mysql.createPool({
     host: process.env.DB_HOST,
